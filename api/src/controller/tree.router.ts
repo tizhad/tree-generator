8d0ce376a2f6ca87263889bfaa4e76886663cb3c
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2023-06-25 19:13:37 +0200

    Remove unused files.

--- api/src/controller/tree.router.ts
+++
@@ -1,6 +0,0 @@
-// defining an endpoint to return all trees data
-app.get("/trees", (req: any, res: any) => {
-    console.log('here')
-    console.log(res)
-    res.send(treesData);
-});
--- api/src/treeModel.ts
+++
@@ -1,6 +0,0 @@
-interface Tree {
-    id: number;
-    name: string;
-    nodes: Tree[];
-}
-
